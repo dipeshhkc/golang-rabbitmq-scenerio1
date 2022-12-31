f97145ec75e40aa47d9760286692d56d31373345
dipesh <kcdipesh429@gmail.com>
2022-12-30 20:43:05 -0600

    feat: add contants

---
+++ constants/constant.go
@@ -0,0 +1,15 @@
+package constants
+
+const (
+	QUEUE = "queue-1"
+
+	USERNAME = "admin"
+	PASSWORD = "admin"
+	HOST     = "localhost"
+	PORT     = "5672"
+
+	TASK1 = "task-1"
+	TASK2 = "task-2"
+
+	SERVERPORT = ":8000"
+)
